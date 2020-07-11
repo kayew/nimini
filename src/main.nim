624009a788a4bdda4b5220f792bbc51f0da93165
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-07-10 23:17:04 -0400

    status code 20: ✔️

--- src/main.nim
+++ src/main.nim
@@ -8,18 +8,24 @@ proc domain(name: string): string =
         return name
 
 var 
+    site: string = domain(paramStr(1))
+    lines: string = "abc"
+    isRunning: bool = true
+
+let 
     s = newSocket()
     c = newContext(protSSLv23, CVerifyNone)
-    site: string = domain(paramStr(1))
-    lines = "abc"
 
 c.wrapSocket(s)
 
 s.connect(site, Port(1965))
 s.send("gemini://" & site & "/index.gmi" & "\r\n")
 
-while lines != "":
-    lines = s.recvLine()
-    stdout.writeLine(lines)
+while isRunning:
+    if lines.len > 0:
+        lines = s.recvLine()
+        echo lines
+    else:
+        break
 
 s.close()
